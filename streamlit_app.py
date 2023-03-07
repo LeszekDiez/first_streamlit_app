be46453f28a1c742284ce95c658bddce1fab6f8b
LeszekDiez <leszek.diez@gmail.com>
2023-03-07 13:20:37 -0300

    kiwi

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
 #streamlit.text(fruityvice_response.json())
 
 # Get the json from the response and transform it to a pandas dataframe 
